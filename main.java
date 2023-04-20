d41f6c7c989e359f383230ff33fefbf27786b9b3
Vipinchaudhary885 <vipinchaudhary13365@gmail.com>
2023-04-19 22:16:09 -0700

    files are added

--- main.java
+++ main.java
@@ -1,6 +1,7 @@
 public class main{
 public static void main (String args []){
 System.out.prinln("Hello world");
-System.out.prinln("Hello all")
+System.out.prinln("Hello all");
+vipin chaudhary
  }
 }
